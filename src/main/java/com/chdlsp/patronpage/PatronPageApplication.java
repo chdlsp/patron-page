e2785040dd79e318f9110646ce4935499a7da13d
Chris <44944612+chdlsp@users.noreply.github.com>
2020-08-28 21:19:24 +0900

    Initial commit

---
+++ src/main/java/com/chdlsp/patronpage/PatronPageApplication.java
@@ -0,0 +1,13 @@
+package com.chdlsp.patronpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatronPageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatronPageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chdlsp/patronpage/PatronPageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chdlsp.patronpage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatronPageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
